16b27336b7eb86d280823c5b9efb01c41594397d
Yyyyyyyao <auyy1997@gmail.com>
2018-05-08 10:12:47 +1000

    Update Car.java

--- Car.java
+++ Car.java
@@ -3,16 +3,14 @@ public class Car {
 
 	private int carId;
 	private int length;
-	private String color;
 	private int direction; // vertical: 1, horizontal: 0;
 	private int posCol;
 	private int posRow;
 	
 	
-	public Car(int carId, int length, String color, int direction, int posCol, int posRow) {
+	public Car(int carId, int length, int direction, int posCol, int posRow) {
 		this.carId = carId;
 		this.length = length;
-		this.color = color;
 		this.direction = direction;
 		this.posCol = posCol;
 		this.posRow = posRow;
@@ -30,10 +28,6 @@ public class Car {
 	
 	public int getLength() {
 		return length;
-	}
-	
-	public String getColor() {
-		return color;
 	}
 	
 	public int getDirection() {
