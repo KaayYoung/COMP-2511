ff35a49d67a512681c69df873c63f8ca247cec3f
Le Pham <30434080+martinpham97@users.noreply.github.com>
2018-05-09 10:19:23 +1000

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,42 +1,64 @@
-import java.util.HashSet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Board {
-	private int board_size;
-	private HashSet<Car> carSet;
+	private int boardSize;
 	private int[][] gameBoard;
+	private HashMap<Integer, Car> cars;
+	private int carCount;
 	
-	public Board() {
-		this.board_size = 6;
-		for (int col = 0; col < board_size; col++) {
-			for (int row = 0; row < board_size; row++) {
-				this.gameBoard[col][row] = 0;
-			}
-		}
+	public Board(HashMap<Integer, Car> cars) {
+		this.boardSize = 6;
+		this.cars = new HashMap<Integer, Car>(cars);
+		this.carCount = cars.size();
 	}
 	
-	public int getBoard_size() {
-		return board_size;
+	public int getBoardSize() {
+		return boardSize;
 	}
 
-	public HashSet<Car> getCarSet() {
-		return carSet;
+	public Car getCar(int carId) {
+		return cars.get(carId);
 	}
-	public void setCarSet(HashSet<Car> carSet) {
-		this.carSet = carSet;
-	}
-	public int[][] getGameBoard() {
-		return gameBoard;
-	}
+	
 	public void setGameBoard(int[][] gameBoard) {
 		this.gameBoard = gameBoard;
 	}
 	
-	public int canMove(Car cars) {
-		int minBlocks = 0;
-		/**
-		 *  to_do
-		 */
-		return minBlocks;
+	public int[][] getGameBoard() {
+		@SuppressWarnings("rawtypes")
+		Iterator i = cars.entrySet().iterator();
+		
+		while(i.hasNext()) {
+			@SuppressWarnings("rawtypes")
+			Map.Entry me = (Map.Entry)i.next();
+			Car currentCar = (Car) me.getValue();
+			if (currentCar.getDirection() == 0) {
+				for (int j = 0; j < currentCar.getLength(); j++) {
+					gameBoard[currentCar.getPosRow() - 1][currentCar.getPosCol() + j] = currentCar.getCarId();
+				}
+			}
+			else {
+				for (int j = 0; j < currentCar.getLength(); j++) {
+					gameBoard[currentCar.getPosRow() - currentCar.getLength() + j][currentCar.getPosCol()] = currentCar.getCarId();
+				}
+			}
+		}
+		
+		return gameBoard;
+	}
+	
+	public ArrayList<Board> getPossibleMoves() {
+		ArrayList<Board> moves = new ArrayList<Board>();
+		
+		int[][] board = getGameBoard();
+		
+		HashMap<Integer, Car> carsCopy = new HashMap<Integer, Car>(getCars());;
+		
+		return moves;
 	}
 	
 	public boolean addCars() {
@@ -45,5 +67,17 @@ public class Board {
 		 *  to_to
 		 */
 		return canAdd;
+	}
+	
+	public boolean isSolved() {
+		return (getCar(1).getPosCol() + getCar(1).getLength() == 6);
+	}
+
+	public int getCarCount() {
+		return carCount;
+	}
+
+	public HashMap<Integer, Car> getCars() {
+		return cars; 
 	}
 }
