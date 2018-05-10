f5dc0e6ead4de00e37dd310ba7b05826ef65a739
Yyyyyyyao <auyy1997@gmail.com>
2018-05-10 16:34:06 +1000

    Update CarMove.java

--- CarMove.java
+++ CarMove.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -7,6 +8,20 @@ import javax.swing.*;
    A program that allows users to move a car with the mouse.
 */
 public class CarMove{
+	
+	static int matrix[][] = {{1,1,0,0,0,0},
+			  		  		 {0,0,0,0,5,0},
+			  		  		 {0,0,6,6,5,0},
+			  		  		 {4,4,4,0,0,0},
+			  		  		 {0,0,0,7,7,7},
+			  		  		 {0,0,0,0,0,0}};
+	
+	private static ArrayList<JLabel> carList = new ArrayList<JLabel>();
+	private static final int Car_width = 150;
+	
+	private static ArrayList<MoveComponent> moveList = new ArrayList<MoveComponent>();
+	
+	
 	public static void main(String[] args){
 		
 		JFrame frame = new JFrame();
@@ -29,21 +44,110 @@ public class CarMove{
 	    
 	    JPanel board = new JPanel();
 	    board.setBounds(250, 300, 900, 900);
-	    board.setLayout(new BorderLayout());
+	    board.setLayout(null);
 	    
 	    board.setBackground(Color.GRAY);
+	    
+		for(int row = 0; row < 6; row++) {
+			for(int col = 0; col < 6; col++) {
+			
+				if(matrix[row][col] != 0) {
+					
+					int num = matrix[row][col];
+					if(col != 5 && matrix[row][col+1] == num) {
+						if(col != 4 && matrix[row][col+2] == num) {
+							// x , y, width, length, direction
+						    JLabel a = new JLabel();
+						    a.setBounds(col*Car_width,row*Car_width,Car_width*3,Car_width);
+						    a.setBackground(Color.BLACK);
+						    a.setOpaque(true);
+							carList.add(a);
+							
+							MoveComponent mc = new MoveComponent();
+							mc.registerComponent(a);
+							mc.setDirection(0);
+							mc.setSnapSize(new Dimension(150,150));
+							
+							moveList.add(mc);
+							col = col + 2;
+						} else {
+						    JLabel a = new JLabel();
+						    a.setBounds(col*Car_width,row*Car_width,Car_width*2,Car_width);
+						    a.setBackground(Color.BLACK);
+						    a.setOpaque(true);
+							carList.add(a);
+							MoveComponent mc = new MoveComponent();
+							mc.registerComponent(a);
+							mc.setDirection(0);
+							mc.setSnapSize(new Dimension(150,150));
+							
+							moveList.add(mc);
+							col = col + 1;
+						}
+					}	
+				}		
+			}
+		}
+		
+		for(int col = 0; col < 6; col++) {
+			for(int row = 0;  row < 6; row++) {
+				
+				if(matrix[row][col] != 0) {
+					
+					int num = matrix[row][col];
+					if(row != 5 && matrix[row+1][col] == num) {
+						if(row != 4 && matrix[row+2][col] == num) {
+						    JLabel a = new JLabel();
+						    a.setBounds(col*Car_width,row*Car_width,Car_width,Car_width*3);
+						    a.setBackground(Color.YELLOW);
+						    a.setOpaque(true);
+							carList.add(a);
+							
+							MoveComponent mc = new MoveComponent();
+							mc.registerComponent(a);
+							mc.setDirection(1);
+							mc.setSnapSize(new Dimension(150,150));
+							
+							moveList.add(mc);
+							row = row + 2;
+						}else {
+						    JLabel a = new JLabel();
+						    a.setBounds(col*Car_width,row*Car_width,Car_width,Car_width*2);
+						    a.setBackground(Color.YELLOW);
+						    a.setOpaque(true);
+							carList.add(a);
+							
+							MoveComponent mc = new MoveComponent();
+							mc.registerComponent(a);
+							mc.setDirection(1);
+							mc.setSnapSize(new Dimension(150,150));
+							moveList.add(mc);
+							row = row + 1;
+						}
+					}
+					
+				}
+				
+			}
+		}
+	     
    
-	    JComponent c1 = new CarComponent();
-
-	    board.add(c1, BorderLayout.CENTER);
+		for(int i = 0; i < carList.size(); i++) {
+			JLabel c = carList.get(i);
+			board.add(c);
+			
+		}
+		
+		
+	    
+//	    JComponent c1 = new CarComponent();
+//
+//	    board.add(c1, BorderLayout.CENTER);
 	     
 	    textPanel.add(board);
 	    
 	    frame.add(textPanel);
-		
-		
-		
-		
+
 		
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setVisible(true);
